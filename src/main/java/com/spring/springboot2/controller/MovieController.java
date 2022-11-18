8709d819d0adacff65b770a01390b20f055923eb
Julius Cavalcante <julius.c@outlook.com.br>
2022-11-18 13:12:31 -0300

    feat: added log42j

--- src/main/java/com/spring/springboot2/controller/MovieController.java
+++ src/main/java/com/spring/springboot2/controller/MovieController.java
@@ -2,22 +2,27 @@ package com.spring.springboot2.controller;
 
 import com.spring.springboot2.domain.Movie;
 import com.spring.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("movie")
+@Log4j2
+@RequiredArgsConstructor
 public class MovieController {
 
-    @Autowired
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
 
     @GetMapping(path = "list")
     public List<Movie> list() {
-        return List.of(new Movie("Batman"), new Movie("The Judge"));
+        log.info(dateUtil.formatLocalDateTimeToDatebaseStyle(LocalDateTime.now()));
+        return List.of(new Movie("The Dark Knight"), new Movie("The Prestige"));
     }
 }
