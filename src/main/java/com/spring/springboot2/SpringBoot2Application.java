b9b5ef82cf9bb1ea27d5dc609cf27b9d6f1784f7
Julius Cavalcante <julius.c@outlook.com.br>
2022-11-18 11:44:09 -0300

    build: SpringBootStarterAplication configuration

--- src/main/java/com/spring/springboot2/SpringBoot2Application.java
+++ src/main/java/com/spring/springboot2/SpringBoot2Application.java
@@ -1,9 +1,11 @@
 package com.spring.springboot2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class SpringBoot2Application {
 
     public static void main(String[] args) {
