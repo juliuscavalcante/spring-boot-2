437de9f30d55dcb7e3dfb9a578316ad17628c897
Julius Cavalcante <julius.c@outlook.com.br>
2022-11-18 13:11:37 -0300

    fix: fix date time format

--- src/main/java/com/spring/springboot2/util/DateUtil.java
+++ src/main/java/com/spring/springboot2/util/DateUtil.java
@@ -9,6 +9,6 @@ import java.time.format.DateTimeFormatter;
 public class DateUtil {
 
     public String formatLocalDateTimeToDatebaseStyle(LocalDateTime localDateTime) {
-        return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(localDateTime);
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
     }
 }
