3fe2d4b5ba66ceab3a5a15f08d178694c15a2f59
Julius Cavalcante <julius.c@outlook.com.br>
2022-11-18 13:56:20 -0300

    feat: findAll and findById implements

--- src/main/java/com/spring/springboot2/controller/MovieController.java
+++ src/main/java/com/spring/springboot2/controller/MovieController.java
@@ -1,11 +1,15 @@
 package com.spring.springboot2.controller;
 
 import com.spring.springboot2.domain.Movie;
+import com.spring.springboot2.service.MovieService;
 import com.spring.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +17,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("movie")
+@RequestMapping("movies")
 @Log4j2
 @RequiredArgsConstructor
 public class MovieController {
 
     private final DateUtil dateUtil;
+    private final MovieService movieService;
 
-    @GetMapping(path = "list")
-    public List<Movie> list() {
+    @GetMapping
+    public ResponseEntity<List<Movie>> findAll() {
         log.info(dateUtil.formatLocalDateTimeToDatebaseStyle(LocalDateTime.now()));
-        return List.of(new Movie("The Dark Knight"), new Movie("The Prestige"));
+        return ResponseEntity.ok(movieService.findAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Movie> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(movieService.findById(id));
     }
 }
--- src/main/java/com/spring/springboot2/service/MovieService.java
+++ src/main/java/com/spring/springboot2/service/MovieService.java
@@ -2,16 +2,27 @@ package com.spring.springboot2.service;
 
 import com.spring.springboot2.domain.Movie;
 import com.spring.springboot2.repository.MovieRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class MovieService {
 
+    private List<Movie> movies = List.of(new Movie(1L,"The Dark Knight"), new Movie(2L,"The Prestige"));
+
     //private final MovieRepository movieRepository;
 
-    public List<Movie> listAll() {
-        return List.of(new Movie(1L,"The Dark Knight"), new Movie(2L,"The Prestige"));
+    public List<Movie> findAll() {
+        return movies;
+    }
+
+    public Movie findById(Long id) {
+        return movies.stream()
+                .filter(movie -> movie.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Movie id not found"));
     }
 }
