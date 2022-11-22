f7a36d3f7aac01639d541095a6d57249cccf7741
Julius Cavalcante <julius.c@outlook.com.br>
2022-11-22 12:45:04 -0300

    feat: Post and Put Request Body class

---
+++ src/main/java/com/spring/springboot2/requests/MoviePostRequestBody.java
@@ -0,0 +1,11 @@
+package com.spring.springboot2.requests;
+
+
+import lombok.Data;
+
+@Data
+public class MoviePostRequestBody {
+
+    private String name;
+
+}
---
+++ src/main/java/com/spring/springboot2/requests/MoviePutRequestBody.java
@@ -0,0 +1,13 @@
+package com.spring.springboot2.requests;
+
+
+import lombok.Data;
+
+@Data
+public class MoviePutRequestBody {
+
+    private Long id;
+
+    private String name;
+
+}
