5034a6a6c78a2bf59cecc690c260d47b847a45a8
Julius Cavalcante <julius.c@outlook.com.br>
2022-12-08 14:22:04 -0300

    feat: add in class @AllArgsConstructor and @NoArgsConstructor

--- src/main/java/com/spring/springboot2/requests/MoviePostRequestBody.java
+++ src/main/java/com/spring/springboot2/requests/MoviePostRequestBody.java
@@ -1,12 +1,17 @@
 package com.spring.springboot2.requests;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MoviePostRequestBody {
 
     @NotBlank(message = "Movie name can't be empty or null")
