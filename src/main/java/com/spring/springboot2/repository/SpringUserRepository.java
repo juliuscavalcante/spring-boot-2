e6502aa0318a2413ba45b5c8d94758e661ea56ae
Julius Cavalcante <julius.c@outlook.com.br>
2022-12-26 11:45:55 -0300

    feat: implements @Repository annotation

--- src/main/java/com/spring/springboot2/repository/SpringUserRepository.java
+++ src/main/java/com/spring/springboot2/repository/SpringUserRepository.java
@@ -2,7 +2,9 @@ package com.spring.springboot2.repository;
 
 import com.spring.springboot2.domain.SpringUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpringUserRepository extends JpaRepository<SpringUser, Long>  {
 
     SpringUser findByUsername(String username);
